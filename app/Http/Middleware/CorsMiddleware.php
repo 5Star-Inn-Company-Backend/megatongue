38e4ef08568e12f64ba21b35f6d009526fe83518
princessvincent <priscavincent2018@gmail.com>
2023-10-10 13:19:24 +0100

    solved cors issue

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -23,7 +23,6 @@ class CorsMiddleware
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
         return $response;
     }
 }
